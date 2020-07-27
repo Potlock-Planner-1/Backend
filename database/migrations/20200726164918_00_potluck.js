9a12ace4055d97ff0aade80bbbdbd407d04cd0e2
unknown <prakashbahadursingh2@gmail.com>
2020-07-27 09:11:57 -0700

    migration complete with user, potluck, item and linked tables

--- database/migrations/20200726164918_00_potluck.js
+++ database/migrations/20200726164918_00_potluck.js
@@ -1,34 +1,106 @@
 
 exports.up = function(knex) {
   return knex.schema
-    .createTable("role", tbl => {
-        tbl. increments();
+    .createTable("role", roleTbl => {
+        roleTbl. increments();
 
-        tbl.string("name", 128)
+        roleTbl.string("name", 128)
             .notNullable()
             .unique();
     })
 
-    .createTable("users", tbl => {
-        tbl.increments();
+    .createTable("users", UsersTbl => {
+        UsersTbl.increments();
 
-        tbl.string("email", 256)
+        UsersTbl.string("username", 256)
             .notNullable()
             .unique()
             .index();
-        tbl.string("password", 256)
+
+        UsersTbl.string("password", 256)
             .notNullable();
 
-        tbl. integer("role")
+        UsersTbl.integer("role_id")
             .unsigned()
-            .reference("roles.id")
-            .onDelete("RESTRICT")
-            .onUpdate("CASCADE")
+            .references("id")
+            .inTable("role")
     })
+
+    .createTable("potluck", potluckTbl => {
+        potluckTbl.increments();
+
+        potluckTbl.string("name", 256)
+                .notNullable()
+                .unique()
+
+        potluckTbl.date("date", 256)
+                .notNullable()
+
+        potluckTbl.time("time", 256)
+                .notNullable()
+
+        potluckTbl.string("location", 256)
+                .notNullable()
+
+        potluckTbl.string("host", 256)
+                .unsigned()
+                .references("id")
+                .inTable("users")
+                .notNullable("CASCADE")
+                .onDelete("CASCADE")
+    })
+
+    .createTable("item", itemTbl => {
+        itemTbl.increments()
+
+        itemTbl.string("item_name", 256)
+                .notNullable()
+
+        itemTbl.integer("claimed")
+                .notNullable()
+
+        itemTbl.integer("potluck_id")
+                .unsigned()
+                .references("id")
+                .inTable("potluck")
+                .notNullable()
+                .onUpdate("CASCADE")
+                .onDelete("RESTRICT")
+    })
+
+    .createTable("linked", linkedTbl => {
+        linkedTbl.integer("item_id")
+                .unsigned()
+                .references("id")
+                .inTable("item")
+                .notNullable()
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE")
+
+        linkedTbl.integer("user_id")
+                .unsigned()
+                .references("id")
+                .inTable("users")
+                .notNullable()
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE")
+
+        linkedTbl.integer("potluck_id")
+                .unsigned()
+                .references("id")
+                .inTable("potluck")
+                .notNullable()
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE")
+    })
+
 };
 
 exports.down = function(knex) {
   return knex.schema
+    .dropTableIfExists("linked")
+    .dropTableIfExists("item")
+    .dropTableIfExists("potluck")
     .dropTableIfExists("roles")
     .dropTableIfExists("users")
 };
--- users/users-router.js
+++ users/users-router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const Users = require("../auth/auth-router.js");
+const Users = require("../auth/auth-model.js");
 
 router.get("/", (req, res) => {
     Users.find()
