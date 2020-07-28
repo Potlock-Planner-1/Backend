65c94044839ec8bac788b3cea7187ef2fd3b2d1e
unknown <prakashbahadursingh2@gmail.com>
2020-07-27 22:11:54 -0700

    food and potluck router and model file completed

--- users/potluck-router.js
+++ users/potluck-router.js
@@ -5,6 +5,19 @@ const jwt = require("jsonwebtoken");
 
 const Potluck = require("./potluck-model.js")
 
+router.post("/", (req, res) => {
+    const postPotluck = req.body;
+
+    Potluck.add(postPotluck)
+        .then(potluck => {
+            res.status(200).json(potluck)
+        })
+        .then(error => {
+            res.status(500).json({message: "failed to create a new potluck"})
+        })
+
+})
+
 router.get("/", (req, res) => {
     Potluck.find()
     .then(potlucks => {
@@ -17,12 +30,53 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
     const { id } = req.params
-    Projects.getByTasksId(id)
-        .then(task => {
-            res.status(200).json(task)
+
+    Potluck.findById(id)
+        .then(potluck => {
+            res.status(200).json(potluck)
         })
         .catch(error => {
             console.log(error)
-            res.status(500).json({message: "Task with the ID can not be retrieved"})
+            res.status(500).json({message: "potluck with the ID does not exists"})
         })
 })
+
+
+router.put("/:id", (req, res) => {
+    const { id } = req.params
+    const change = req.body
+
+    Potluck.findById(id)
+        .then(potluck => {
+            if(potLuck){
+                Potluck.update(change, id)
+                    .then(updatePotluck => {
+                        res.status(200).json(updatePotluck)
+                    })
+            }else {
+                res.status(404).json({message: "potluck with id can not be found"})
+            }
+        })
+        .catch (error => {
+            res.status(500).json({ message: "potluck info can not be updated"})
+        })
+})
+
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+
+    Potluck.remove(id)
+        .then(potluck => {
+        if (potluck) {
+            res.status(204).json({ removed: deleted});
+        } else {
+            res.status(404).json({ message: 'potluck with id could not be found' });
+        }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "failed to delete the potluck"});
+        });
+  });
+  
+  module.exports = server;
