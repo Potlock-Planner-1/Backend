127158918fbd7cc2b0d405e8aef1ee712844eb00
Prakash Bahadur Singh <prakashbahadursingh2@gmail.com>
2021-02-05 00:05:17 -0800

    instruction added to the base url to check backend server working

--- api/server.js
+++ api/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const morgan = require("morgan");
-const cors = require('cors');
-const helmet = require('helmet');
+const cors = require("cors");
+const helmet = require("helmet");
 
 const authRouter = require("../auth/auth-router.js");
 const usersRouter = require("../users/users-router.js");
 const potluckRouter = require("../users/potluck-router.js");
 const itemRouter = require("../users/items-router.js");
-const guestRouter = require("../users/guests-router.js")
+const guestRouter = require("../users/guests-router.js");
 
 const authenticate = require("../auth/authenticate-middleware.js");
 
@@ -18,15 +18,19 @@ server.use(morgan("dev"));
 server.use(cors());
 server.use(express.json());
 
-server.use("/api/auth", authRouter)
-server.use("/api/users",authenticate, usersRouter)
-server.use("/api/potlucks", potluckRouter)
-server.use("/api/items", itemRouter)
-server.use("/api/guests", guestRouter)
-
+server.use("/api/auth", authRouter);
+server.use("/api/users", authenticate, usersRouter);
+server.use("/api/potlucks", potluckRouter);
+server.use("/api/items", itemRouter);
+server.use("/api/guests", guestRouter);
 
 server.get("/", (req, res) => {
-    res.json({ api: "We Are In Backend Build Week. Server Is Up And Running............................" });
+  res.json({
+    api:
+      "This Is Backend Server and It Is Up And Running............................", 
+      -->:"  ",
+    routes: "/potlucks, /guests, /items, /users",
   });
+});
 
 module.exports = server;
